4721d06537cb9ddbfdc14cf321b736cbc6c3558a
Shahrukh Malik <srmrox@live.com>
2019-05-16 13:47:29 +0500

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+    }
+};
