f7b65e53081ba834b4e9ff77db2d5dd28c269cb9
Shahrukh Malik <srmrox@live.com>
2019-05-16 22:44:55 +0500

    added start script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'consumerKey'       : 'ADD IN YOUR KEY',
-    'consumerSecret'    : 'ADD IN YOUR SECRET',
+    'consumerKey'       : 'ADD YOUR KEY HERE',
+    'consumerSecret'    : 'ADD YOUR SECRET HERE',
     'callbackURL'       : 'http://127.0.0.1:8080/signin'
 };
